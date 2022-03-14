44f1801f869da9b398a05ed82e727dc7795ea0c8
Debbie <hifedebbie@gmail.com>
2022-03-14 23:02:25 +0100

    First javascript project

---
+++ Counter/index.js
@@ -0,0 +1,31 @@
+
+ let countEl = document.getElementById("count-el")
+
+ let saveEl = document.getElementById("save-el")
+ 
+ let count = 0;
+ 
+function increment(){
+    count += 1
+   countEl.innerText = count
+}
+
+ 
+function decrement(){
+    if(count === 0){
+        count -= 0
+    } else {
+        count -= 1
+      
+    }
+   countEl.innerText = count
+}
+
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText  += countStr
+    countEl.innerText  = 0
+    count = 0
+}
